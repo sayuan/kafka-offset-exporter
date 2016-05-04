9a384eddef6d6bd3ee6bef7e3bdc667029eebd1b
sayuan <shiao.an.yuan@gmail.com>
2016-05-05 00:34:28 +0800

    [debug] Catch unknown exception

--- src/main/scala/net/sayuan/kafka/KafkaOffsetUpdater.scala
+++ src/main/scala/net/sayuan/kafka/KafkaOffsetUpdater.scala
@@ -16,51 +16,56 @@ class KafkaOffsetUpdater (config: Config) extends Runnable {
   val groupTimestampGauge = Gauge.build().name("kafka_group_timestamp").help("group timestamp").labelNames("topic", "partition", "group").register()
 
   def run() {
-    val zkClient = new ZkClient(config.zookeeper, 30000, 30000, new ZKStringSerializer())
-    val zkUtils = ZkUtils(zkClient, false)
+    try {
+      val zkClient = new ZkClient(config.zookeeper, 30000, 30000, new ZKStringSerializer())
+      val zkUtils = ZkUtils(zkClient, false)
 
-    for (group <- zkUtils.getConsumerGroups();
-         (topic, partitions) <- zkUtils.getPartitionsForTopics(zkUtils.getTopicsByConsumerGroup(group));
-         pid <- partitions) {
+      for (group <- zkUtils.getConsumerGroups();
+           (topic, partitions) <- zkUtils.getPartitionsForTopics(zkUtils.getTopicsByConsumerGroup(group));
+           pid <- partitions) {
 
-      val (offset, stat) = zkUtils.readDataMaybeNull(s"${ZkUtils.ConsumersPath}/$group/offsets/$topic/$pid")
-      if (offset.isDefined) {
-        groupOffsetGauge.labels(topic, pid.toString, group).set(offset.get.toDouble)
-        groupTimestampGauge.labels(topic, pid.toString, group).set(stat.getMtime)
-      }
-    }
-
-    for (topic <- zkUtils.getAllTopics()) {
-      for (pid <- zkUtils.getPartitionsForTopics(Seq(topic))(topic)) {
-        val topicAndPartition = TopicAndPartition(topic, pid)
-
-        zkUtils.getLeaderForPartition(topic, pid) match {
-          case Some(bid) =>
-
-            zkUtils.readDataMaybeNull(s"${ZkUtils.BrokerIdsPath}/$bid") match {
-              case (Some(brokerInfoString), _) =>
-                Json.parseFull(brokerInfoString) match {
-                  case Some(m) =>
-                    val brokerInfo = m.asInstanceOf[Map[String, Any]]
-                    val host = brokerInfo.get("host").get.asInstanceOf[String]
-                    val port = brokerInfo.get("port").get.asInstanceOf[Int]
-                    val consumer = new SimpleConsumer(host, port, 10000, 100000, "KafkaOffsetExporter")
-                    val request = OffsetRequest(Map(topicAndPartition -> PartitionOffsetRequestInfo(OffsetRequest.LatestTime, 1)))
-                    val logSize = consumer.getOffsetsBefore(request).partitionErrorAndOffsets(topicAndPartition).offsets.head
-                    consumer.close()
-                    logEndOffsetSizeGauge.labels(topic, pid.toString).set(logSize)
-                  case None =>
-                    logger.warn("Broker id %d does not exist".format(bid))
-                }
-              case (None, _) =>
-                logger.warn("Broker id %d does not exist".format(bid))
-            }
-          case None =>
-            logger.warn("No broker for partition %s - %s".format(topic, pid))
+        val (offset, stat) = zkUtils.readDataMaybeNull(s"${ZkUtils.ConsumersPath}/$group/offsets/$topic/$pid")
+        if (offset.isDefined) {
+          groupOffsetGauge.labels(topic, pid.toString, group).set(offset.get.toDouble)
+          groupTimestampGauge.labels(topic, pid.toString, group).set(stat.getMtime)
         }
       }
-    }
 
-    zkUtils.close()
+      for (topic <- zkUtils.getAllTopics()) {
+        for (pid <- zkUtils.getPartitionsForTopics(Seq(topic))(topic)) {
+          val topicAndPartition = TopicAndPartition(topic, pid)
+
+          zkUtils.getLeaderForPartition(topic, pid) match {
+            case Some(bid) =>
+
+              zkUtils.readDataMaybeNull(s"${ZkUtils.BrokerIdsPath}/$bid") match {
+                case (Some(brokerInfoString), _) =>
+                  Json.parseFull(brokerInfoString) match {
+                    case Some(m) =>
+                      val brokerInfo = m.asInstanceOf[Map[String, Any]]
+                      val host = brokerInfo.get("host").get.asInstanceOf[String]
+                      val port = brokerInfo.get("port").get.asInstanceOf[Int]
+                      val consumer = new SimpleConsumer(host, port, 10000, 100000, "KafkaOffsetExporter")
+                      val request = OffsetRequest(Map(topicAndPartition -> PartitionOffsetRequestInfo(OffsetRequest.LatestTime, 1)))
+                      val logSize = consumer.getOffsetsBefore(request).partitionErrorAndOffsets(topicAndPartition).offsets.head
+                      consumer.close()
+                      logEndOffsetSizeGauge.labels(topic, pid.toString).set(logSize)
+                    case None =>
+                      logger.warn("Broker id %d does not exist".format(bid))
+                  }
+                case (None, _) =>
+                  logger.warn("Broker id %d does not exist".format(bid))
+              }
+            case None =>
+              logger.warn("No broker for partition %s - %s".format(topic, pid))
+          }
+        }
+      }
+
+      zkUtils.close()
+
+    } catch {
+      case e: Exception => e.printStackTrace()
+    }
   }
 }
